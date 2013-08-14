2aa7b29caae8cf3cee8b55c4d95975e80d2c799b
Suki <suki@missallsunday.com>
2013-08-14 16:09:58 -0500

    The edit on Display.template
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/showCustomMembergroups.english.php
+++ Themes/default/languages/showCustomMembergroups.english.php
@@ -15,5 +15,3 @@ $txt['sCM_title'] = 'Show custom membergroups';
 $txt['sCM_setting_pageTitle'] = 'SCM admin page';
 $txt['sCM_groups_ids'] = 'Put the groups IDs you want to show for each user';
 $txt['sCM_groups_ids_sub'] = 'comma separated (1,2,3,4), leave empty if you want to show all membergroups.';
-$txt['sCM_'] = '';
-$txt['sCM_'] = '';
